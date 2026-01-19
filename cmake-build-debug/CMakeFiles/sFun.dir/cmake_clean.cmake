5d16dfdab8cdff8d531e7e826c9a4b6eba7fef2d
Alejandro <alecoco09j@gmail.com>
2026-01-18 21:04:25 -0500

    change of class

--- cmake-build-debug/CMakeFiles/sFun.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/sFun.dir/cmake_clean.cmake
@@ -1,8 +1,12 @@
 file(REMOVE_RECURSE
   "CMakeFiles/sFun.dir/src/CPU.cpp.obj"
   "CMakeFiles/sFun.dir/src/CPU.cpp.obj.d"
+  "CMakeFiles/sFun.dir/src/Cartridge.cpp.obj"
+  "CMakeFiles/sFun.dir/src/Cartridge.cpp.obj.d"
   "CMakeFiles/sFun.dir/src/Memory.cpp.obj"
   "CMakeFiles/sFun.dir/src/Memory.cpp.obj.d"
+  "CMakeFiles/sFun.dir/src/cart.cpp.obj"
+  "CMakeFiles/sFun.dir/src/cart.cpp.obj.d"
   "CMakeFiles/sFun.dir/src/main.cpp.obj"
   "CMakeFiles/sFun.dir/src/main.cpp.obj.d"
   "libsFun.dll.a"
--- include/cart.h
+++ include/cart.h
@@ -6,15 +6,32 @@
 #define CART_H
 #include "stdio.h"
 #include <stdint.h>
+#include <vector>
 #pragma once
 
 // switch to vectors and enums
-struct Cart{
-  uint8_t type;
-    uint8_t *rom;
-    uint8_t * ram;
+class Cart{
+    public:
+    std::vector<uint8_t> rom;
+    std::vector<uint8_t> sram;
+
     uint32_t rom_size;
-    uint32_t ram_size;
+    uint32_t sram_size;
+
+    bool lorom;
+    bool hirom;
+
+    uint8_t rom_type;
+    uint8_t country;
+    uint8_t license;
+    uint8_t version;
+
+    uint16_t reset_vector;
+    uint16_t nmi_vector;
+
+    uint8_t read8(uint32_t addr);
+    void write8(uint32_t addr, uint8_t value);
+
 };
 
 
